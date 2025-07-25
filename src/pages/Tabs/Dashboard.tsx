e258e358e4d7d50f1dab9025ef1096f6ab1e2053
Roger0222 <20211113@nbsc.edu.ph>
2025-07-25 09:17:55 +0800

    added tabs

---
+++ src/pages/Tabs/Dashboard.tsx
@@ -0,0 +1,28 @@
+import { 
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonMenuButton, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react';
+  
+  const Dashboard: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Dashboard</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default Dashboard;
